f196d0cd03b8e089a7a06c65b7c91f4a87b5c820
Maroof Sheikh <Maroof.Sheikh@communitybrands.com>
2019-10-16 17:48:25 +0530

    Pushing

---
+++ TMS/Controllers/LoginController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMS.Models;
+
+namespace TMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        LoginPage st;
+        Boolean flag,flag1;
+       
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+           
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(LoginPage st)
+        {
+            int[] results=new int[2];
+            flag = st.Login();
+            
+            if (flag == true)
+            {
+                return RedirectToAction("changePassword");
+            }
+            if ( flag == false)
+            {
+                return RedirectToAction("Dashboard");
+            }
+           if(results[1]==2)
+            {
+                return RedirectToAction("error");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult changePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult changePassword( LoginPage st)
+        {
+             flag1 = st.ChangePassword();
+            if (flag1 == true)
+               return  RedirectToAction("Profiles");
+            else
+               return RedirectToAction("error");
+            
+            return View();
+        }
+        
+        public ActionResult error()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Profiles()
+        {
+            if (Session["user_id"] == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Profiles(LoginPage profile)
+        {
+            Boolean flag = profile.UpdateProfile();
+            if (flag == true)
+                return RedirectToAction("Index");
+            else
+                return RedirectToAction("error");
+            /*      string FileName = Path.GetFileNameWithoutExtension(profile.ImageFile.FileName);
+
+                  //To Get File Extension  
+                  string FileExtension = Path.GetExtension(profile.ImageFile.FileName);
+
+                  //Add Current Date To Attached File Name  
+                  FileName = DateTime.Now.ToString("yyyyMMdd") + "-" + FileName.Trim() + FileExtension;
+
+                  //Get Upload path from Web.Config file AppSettings.  
+                  string UploadPath = ConfigurationManager.AppSettings["UserImagePath"].ToString();
+
+                  //Its Create complete path to store in server.  
+                  profile.ImagePath = UploadPath + FileName;
+
+                  //To copy and save file into server.  
+                  profile.ImageFile.SaveAs(profile.ImagePath);
+
+                  //profile.ImageFile=
+                  int Session_id = 0;
+                      if (Session["user_id"] != null)
+                      {
+                          Session_id = Convert.ToInt32(Session["user_id"].ToString());
+
+                      }
+                      training_management_systemEntities entities = new training_management_systemEntities();
+                      company_employee emp = entities.company_employee.FirstOrDefault(e => e.user_id == Session_id);
+
+                      emp.Photograph = profile.ImagePath;
+                      entities.SaveChanges();
+                  }
+
+              else
+              {
+                  return RedirectToAction("error");
+              }*/
+
+
+        
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            if (Session["user_id"] == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            if (Session["user_id"] == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Session.Abandon();
+                Session.Clear();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ TMS/Models/LoginPage.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+using IdentityModel.Client;
+using System.ComponentModel;
+
+namespace TMS.Models
+{
+    public class LoginPage
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public int ManagerId { get; set; }
+
+        [DisplayName("Upload File")]
+        public string ImagePath { get; set; }
+
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        [Required]
+        public DateTime Date_Of_Joining { get; set; }
+
+
+
+
+
+       
+        Boolean flag1,flag;
+        public Boolean Login()
+        {
+            int[] results =new int[2];
+            results[0] = 1;
+            flag = false;
+            training_management_systemEntities entities = new training_management_systemEntities();
+            company_employee emp = entities.company_employee.SingleOrDefault(e => e.username == UserName & e.password==Password);
+
+
+            if (emp != null)
+            {
+                HttpContext.Current.Session["user_id"] = emp.user_id;
+                if (emp.password == "12345")
+                    //results[1] = 0;
+                    flag = true;/*
+                else
+                    results[1] = 1;*/
+
+            }
+            else
+                flag = false;
+
+            return flag;
+        }
+
+
+        public Boolean ChangePassword()
+        {
+
+            flag1 = false;
+            int Session_id=0;
+            if (HttpContext.Current.Session["user_id"] != null)
+            {
+                Session_id =Convert.ToInt32( HttpContext.Current.Session["user_id"].ToString());
+
+            }
+            training_management_systemEntities entities = new training_management_systemEntities();
+            company_employee emp = entities.company_employee.SingleOrDefault(e => e.user_id == Session_id);
+            
+            emp.password = Password;
+            entities.SaveChanges();
+            if (emp.password != "12345")
+                flag1 = true;
+
+            return flag1;
+        }
+
+
+        public Boolean UpdateProfile()
+        {
+            Boolean flag2 = false;
+            int Session_id = 0;
+            if (HttpContext.Current.Session["user_id"] != null)
+            {
+                Session_id = Convert.ToInt32(HttpContext.Current.Session["user_id"].ToString());
+
+            }
+            training_management_systemEntities entities = new training_management_systemEntities();
+            company_employee emp = entities.company_employee.SingleOrDefault(e => e.user_id == Session_id);
+            emp.user_full_name =FullName;
+            emp.user_title = Title;
+            emp.user_department = Department;
+            emp.manager_id = ManagerId;
+            emp.date_of_joining = Date_Of_Joining;
+
+
+            entities.SaveChanges();
+            if (emp != null)
+                flag2 = true;
+
+            return flag2;
+
+        }
+    }
+}
